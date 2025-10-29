c667c32d9cb6ecb394179365bcaf13d238ce106b
michaelEmeka <mikelonu15@gmail.com>
2025-10-29 12:24:10 +0100

    Update ssl cert autherntication method using base64 for multiline encoding and decoding

--- app/database.py
+++ app/database.py
@@ -7,25 +7,35 @@ import tempfile
 import atexit
 import ssl
 from sqlalchemy import text
+import base64
+
 
 load_dotenv()
 
-# from dotenv import load_dotenv
+
+# using encoded ssl certification due to line break error in environment variable in deployment environment
+DB_SSL_CA_B64 = os.getenv("DB_SSL_CA_B64")
+if not DB_SSL_CA_B64:
+    raise RuntimeError("Missing DB_SSL_CA_B64")
+
+decoded_ca = base64.b64decode(DB_SSL_CA_B64)
+
+print(decoded_ca)
+
+temp_ca_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pem")
+temp_ca_file.write(decoded_ca)
+temp_ca_file.flush()
+# atexit.register(lambda: os.remove(temp_ca_file.name))
 
 # print(DATABASE_URL)
 DATABASE_URL = os.getenv("DATABASE_URL")
-#print(os.getenv("DEBUG"))
-#DATABASE_URL = ""
-DB_SSL_CA = os.getenv("DB_SSL_CA")
+# print(os.getenv("DEBUG"))
+# DATABASE_URL = ""
+# DB_SSL_CA = os.getenv("DB_SSL_CA")
 
-if not DB_SSL_CA:
-    print("Missing DB_SSL_CA")
-
-
-
-temp_ca_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pem")
-temp_ca_file.write(DB_SSL_CA.encode("utf-8"))
-temp_ca_file.flush()
+# temp_ca_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pem")
+# temp_ca_file.write(DB_SSL_CA.encode("utf-8"))
+# temp_ca_file.flush()
 
 
 engine = create_engine(
@@ -46,7 +56,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-#db = SessionLocal()
-#print(db.execute(text("SELECT NOW();")).fetchone())
-#db.close()
-#db = SessionLocal()
+# db = SessionLocal()
+# print(db.execute(text("SELECT NOW();")).fetchone())
+# db.close()
+# db = SessionLocal()
